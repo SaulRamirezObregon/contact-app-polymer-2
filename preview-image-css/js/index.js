0934a1436a4577f138375301779ab8027cb875ae
saul.ramirez <saul.ramirez@sngular.team>
2018-08-08 15:03:56 -0500

    estructura

---
+++ preview-image-css/js/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#myfile');
+const preview = document.querySelector('.preview');
+
+input.addEventListener('change', (e) =>{
+  const reader = new FileReader();
+  reader.onload = (e) => preview.setAttribute('src', e.target.result);
+  reader.readAsDataURL(input.files[0]);
+});
